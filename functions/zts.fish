b73286888abbd482c2737517c2fef15cb80b70e5
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2024-05-03 22:42:51 -0700

    fixed zts

--- functions/zts.fish
+++ functions/zts.fish
@@ -1,8 +1,12 @@
-function zts --argument-names worktree
-    if test -z "$worktree"
-        echo "worktree parameter required"
+function zts --description "open zellij tab to worktree layout"
+		cd ~/projects/siggen
+    set common (path resolve (git rev-parse --git-common-dir))
+    if test -z "$common"
         return
     end
 
-		zellij action new-tab -n $worktree -c ~/projects/siggen/$worktree -l siggen-worktree.kd
+    set siggenRoot (path dirname "$common")
+    set name (git worktree list | awk '{print $1 " " $3}' | rg {$siggenRoot}'/(.*)' -or '$1' | fzf | awk '{print $1}')
+
+		zellij action new-tab -n $name -c ./$name -l siggen-worktree
 end
