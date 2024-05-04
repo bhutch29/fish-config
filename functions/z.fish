cc91710a246b8fd73c06f3e3ae5d8c8318905ec0
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2024-05-03 23:19:00 -0700

    fix zts whitespace, improve z

--- functions/z.fish
+++ functions/z.fish
@@ -1,3 +1,8 @@
-function z
-  command zellij --layout welcome
+function z --argument-names session --description "zellij session creator"
+  if test -z "$session"
+    command zellij --layout welcome
+    return
+  end
+
+  command zellij -l $session -s $session
 end
--- functions/zts.fish
+++ functions/zts.fish
@@ -1,22 +1,22 @@
 function zts --description "open zellij tab to worktree layout"
-		if test -z "$ZELLIJ"
-				echo "zts is only valid inside a zellij session"
-				return 1
-		end
+    if test -z "$ZELLIJ"
+        echo "zts is only valid inside a zellij session"
+        return 1
+    end
 
-		pushd ~/projects/siggen
+    pushd ~/projects/siggen
     set common (path resolve (git rev-parse --git-common-dir))
     if test -z "$common"
-				popd
+        popd
         return 1
     end
 
     set siggenRoot (path dirname "$common")
     set name (git worktree list | awk '{print $1 " " $3}' | rg {$siggenRoot}'/(.*)' -or '$1' | fzf | awk '{print $1}')
     if test -z "$name"
-				popd
+        popd
         return 1
     end
 
-		zellij action new-tab -n $name -c ./$name -l siggen-worktree
+    zellij action new-tab -n $name -c ./$name -l siggen-worktree
 end
