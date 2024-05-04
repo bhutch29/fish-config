240f6c4d1f1706a8ea4ad2339d21bfd63c6abe2c
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2024-05-03 22:52:03 -0700

    zts upgrades

--- functions/zts.fish
+++ functions/zts.fish
@@ -1,12 +1,22 @@
 function zts --description "open zellij tab to worktree layout"
-		cd ~/projects/siggen
+		if test -z "$ZELLIJ"
+				echo "zts is only valid inside a zellij session"
+				return 1
+		end
+
+		pushd ~/projects/siggen
     set common (path resolve (git rev-parse --git-common-dir))
     if test -z "$common"
-        return
+				popd
+        return 1
     end
 
     set siggenRoot (path dirname "$common")
     set name (git worktree list | awk '{print $1 " " $3}' | rg {$siggenRoot}'/(.*)' -or '$1' | fzf | awk '{print $1}')
+    if test -z "$name"
+				popd
+        return 1
+    end
 
 		zellij action new-tab -n $name -c ./$name -l siggen-worktree
 end
